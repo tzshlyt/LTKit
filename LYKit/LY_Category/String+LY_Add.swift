d171622ec546f3a037bc1908ccbe7f918c42c33b
ytlan <ytlan0201@gmail.com>
2021-11-18 14:15:49 +0800

    add string category

---
+++ LYKit/LY_Category/String+LY_Add.swift
@@ -0,0 +1,13 @@
+//
+//  String+LY_Add.swift
+//  LYKit
+//
+//  Created by lan on 11/18/21.
+//
+import Foundation
+
+extension String {
+    var localized: String {
+        return NSLocalizedString(self, comment: "")
+    }
+}
